85cc28c6770c50f150d9295bf701a03ab829462a
U180709060 <mmbattaloglu@gmail.com>
2021-11-11 20:24:26 +0300

    Initial commit

---
+++ app/src/main/java/com/mbattaloglu/mymoviebrowser/DetailsFragment.java
@@ -0,0 +1,70 @@
+package com.mbattaloglu.mymoviebrowser;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class DetailsFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "movie";
+
+    private Movie movie;
+
+    public DetailsFragment() {
+        // Required empty public constructor
+    }
+
+
+    // TODO: Rename and change types and number of parameters
+    public static DetailsFragment newInstance(Movie movie) {
+        DetailsFragment fragment = new DetailsFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_PARAM1, movie);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            movie = (Movie) getArguments().getSerializable(ARG_PARAM1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setMovie(view,movie);
+    }
+
+    public void setMovie(View view, Movie movie) {
+        TextView txtName = (TextView)view.findViewById(R.id.txtMovieName);
+        txtName.setText(movie.getName());
+        TextView txtYear = (TextView)view.findViewById(R.id.txtYear);
+        txtYear.setText(Integer.toString(movie.getYear()));
+        TextView txtDirector = (TextView)view.findViewById(R.id.txtDirector);
+        txtDirector.setText(movie.getDirector());
+        TextView txtDescription = (EditText)view.findViewById(R.id.txtDescription);
+        txtDescription.setText(movie.getDescription());
+        txtDescription.setEnabled(false);
+        ListView listView = (ListView) view.findViewById(R.id.lstStars);
+        listView.setAdapter(new ArrayAdapter<String>(getActivity(), R.layout.array_adapter, movie.getStars().toArray(new String[1])));
+    }
+}
---
+++ app/src/main/java/com/mbattaloglu/mymoviebrowser/MyMovieRecyclerViewAdapter.java
@@ -0,0 +1,72 @@
+package com.mbattaloglu.mymoviebrowser;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.mbattaloglu.mymoviebrowser.databinding.FragmentMovieBinding;
+
+import java.util.List;
+
+public class MyMovieRecyclerViewAdapter extends RecyclerView.Adapter<MyMovieRecyclerViewAdapter.ViewHolder> {
+
+    private final List<Movie> mValues;
+    private final MovieFragment.MovieListener mListener;
+    int selectedIndex = -1;
+
+    public MyMovieRecyclerViewAdapter(List<Movie> mValues, MovieFragment.MovieListener mListener) {
+        this.mValues = mValues;
+        this.mListener = mListener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        return new ViewHolder(FragmentMovieBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(position+ 1 + "");
+        holder.mContentView.setText(mValues.get(position).getName());
+        holder.mContentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mListener != null){
+                    mListener.movieSelected(holder.mItem);
+                    notifyItemChanged(selectedIndex);
+                    selectedIndex = holder.getLayoutPosition();
+                    notifyItemChanged(selectedIndex);
+                }
+            }
+        });
+        holder.itemView.setBackgroundColor(selectedIndex == position ? Color.GREEN : Color.TRANSPARENT);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public Movie mItem;
+
+        public ViewHolder(FragmentMovieBinding binding) {
+            super(binding.getRoot());
+            mIdView = binding.itemNumber;
+            mContentView = binding.content;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
